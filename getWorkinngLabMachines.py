40f531058bc9eb8e29be2d3a2369fc733f935cd2
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-02-13 01:05:21 +0000

    Clean up in script which gets thw working machines. Functionality divided into blocks

--- getWorkinngLabMachines.py
+++ getWorkinngLabMachines.py
@@ -1,21 +1,33 @@
 import subprocess
 import re
 
-condor_output = subprocess.check_output("condor_status", shell=True)
-hostnames = []
-EXTENSION = ".doc.ic.ac.uk"
+
+def getAllMachines():
+  condor_output = subprocess.check_output("condor_status", shell=True)
+  hostnames = []
+  for line in condor_output.split("\n"):
+    if re.search("doc", line):
+      hostname = line.split()[0]
+      hostnames += [re.split("\W+", hostname)[0]]
+  return hostnames
 
 
-for line in condor_output.split("\n"):
-  if re.search("doc", line):
-    hostname = line.split()[0]
-    hostnames += [re.split("\W+", hostname)[0]]
+def getWorningMachines(hostnames=None, extension=None):
+  if not hostnames:
+    hostnames = getAllMachines()
+  if not extension:
+    extension = ".doc.ic.ac.uk"
+  workingHostames = []
+  for h in hostnames:
+    h += extension
+    try:
+      subprocess.check_output(["ping", "-c", "1", h], stderr=subprocess.PIPE)
+    except subprocess.CalledProcessError, e:
+      pass
+    else:
+      workingHostames += [h]
+      print h
+  return workingHostames
 
-for h in hostnames:
-  h += EXTENSION
-  try:
-    subprocess.check_output(["ping", "-c", "1", h], stderr=subprocess.PIPE)
-  except subprocess.CalledProcessError, e:
-    pass
-  else:
-    print h
+if __name__ == '__main__':
+  print getWorningMachines()
