b129f7840957ba5029d961725cb13fa6e594e9e3
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-02-13 20:54:19 +0000

    Server that runs the function which gets all the machines

--- trialServer.py
+++ trialServer.py
@@ -2,9 +2,11 @@ import SocketServer
 import getWorkingLabMachines
 
 class Handler(SocketServer.BaseRequestHandler):
+
   def handle(self):
     print self.request
-    self.request.sendall("\n".join(getWorkingLabMachines.getWorkingMachines()))
+    machines = "\n".join(getWorkingLabMachines.getWorkingMachines())
+    self.request.sendall(machines)
 
 web_server = SocketServer.TCPServer(("localhost", 8081), Handler)
 web_server.serve_forever()
